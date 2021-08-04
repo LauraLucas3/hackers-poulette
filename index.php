0fdc0a151c74f1cf6cc0ec1739c75e83c8c0f309
Laura Lucas <webdev.lauralucas@gmail.com>
2021-08-04 14:43:52 +0200

    ajout de la police d'écriture et test de bulma

--- index.php
+++ index.php
@@ -7,22 +7,27 @@
         <h1>Contact support</h1>
 
         <form>
-            <div>
-                <div>
+            <div class="columns">
+                <div class="column">
                     <p>Name</p>
                     <input name="name" type="text" placeholder="John">
                 </div>
-                <div>
+
+                <div class="column">
                     <p>Lastname</p>
                     <input name="lastname" type="text" placeholder="Doe">
                 </div>
             </div>
-            <div>
-                <p>Email address</p>
-                <input name="email" type="text" placeholder="e.g. johndoe@exemple.be">
+
+            <div class="columns">
+                <div class="column">
+                    <p>Email address</p>
+                    <input name="email" type="text" placeholder="e.g. johndoe@exemple.be">
+                </div>
             </div>
-            <div>
-                <div>
+
+            <div class="columns">
+                <div class="column">
                     <p>Gender</p>
                     <input type="radio" name="man">
                     <p>Man</p>
@@ -31,7 +36,8 @@
                     <input type="radio" name="otherg">
                     <p>Other</p>
                 </div>
-                <div>
+
+                <div class="column">
                     <p>Country</p>
                     <select name="country">
                         <option value="default">Select a country</option>
@@ -46,18 +52,24 @@
                     </select>
                 </div>
             </div>
-            <div>
-                <p>Subject</p>
-                <select name="subject">
-                   <option>Other</option>
-                   <option>Infos</option> 
-                   <option>Request</option> 
-                   <option>Joining us</option>
-                </select>
+
+            <div class="columns">
+                <div class="column">
+                    <p>Subject</p>
+                    <select name="subject">
+                        <option>Other</option>
+                        <option>Infos</option> 
+                        <option>Request</option> 
+                        <option>Joining us</option>
+                    </select>
+                </div>
             </div>
-            <div>
-                <p>Message</p>
-                <textarea name="message" placeholder="Write your message here"></textarea>
+
+            <div class="columns">
+                <div class="column">
+                    <p>Message</p>
+                    <textarea name="message" placeholder="Write your message here"></textarea>
+                </div>
             </div>
         </form>
 
