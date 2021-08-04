42269539383be548cc56f02ae87bd049f868d171
Laura Lucas <webdev.lauralucas@gmail.com>
2021-08-04 17:03:39 +0200

    First day finished

--- index.php
+++ index.php
@@ -8,71 +8,78 @@
     </head>
 
     <body>
-        <h1 class="title is-1">Contact support</h1>
+        <div class="titleAndLogo">
+            <img class="logoHackers" src="assets/img/hackers-poulette-logo.png" alt="logo des Hackers poulette">
+            <h1 class="title is-1">Contact support</h1>
+        </div>
 
         <form>
             <div class="columns is-centered is-gapless">
                 <div class="column is-centered is-one-fifth">
-                    <p>Name</p>
-                    <input name="name" type="text" placeholder="John">
+                    <p class="inputLabel">Name</p>
+                    <input class="input is-rounded" name="name" type="text" placeholder="John">
                 </div>
 
                 <div class="column is-centered is-one-fifth">
-                    <p>Lastname</p>
-                    <input name="lastname" type="text" placeholder="Doe">
+                    <p class="inputLabel">Lastname</p>
+                    <input class="input is-rounded" name="lastname" type="text" placeholder="Doe">
                 </div>
             </div>
 
             <div class="columns is-centered is-gapless">
                 <div class="column is-two-fifths">
-                    <p>Email address</p>
-                    <input name="email" type="text" placeholder="e.g. johndoe@exemple.be">
+                    <p class="inputLabel">Email address</p>
+                    <input class="input is-rounded" name="email" type="text" placeholder="e.g. johndoe@exemple.be">
                 </div>
             </div>
 
             <div class="columns is-centered is-gapless">
                 <div class="column is-centered is-one-fifth">
-                    <p>Gender</p>
+                    <p class="genderp">Gender</p><br>
                     <input type="radio" name="man">
-                    <p>Man</p>
+                    <p class="genderp">Man</p><br>
                     <input type="radio" name="woman">
-                    <p>Woman</p>
+                    <p class="genderp">Woman</p><br>
                     <input type="radio" name="otherg">
-                    <p>Other</p>
+                    <p class="genderp">Other</p>
                 </div>
 
                 <div class="column is-centered is-one-fifth is-gapless">
                     <p>Country</p>
-                    <select name="country">
-                        <option value="default">Select a country</option>
-                        <?php
+                    <div class="select is-rounded">
+                        <select name="country">
+                            <option value="default" selected>Select a country</option>
+                            <?php
 
-                            require "countries.php";
-                            
-                            foreach($countries as $key => $value) {
-                                echo "<option value='$key'>$value</option>";
-                            }
-                        ?>
-                    </select>
+                                require "countries.php";
+                                    
+                                foreach($countries as $key => $value) {
+                                    echo "<option value='$key'>$value</option>";
+                                }
+                            ?>
+                        </select>
+                    </div>
                 </div>
             </div>
 
             <div class="columns is-centered is-gapless">
-                <div class="column is-centered is-one-fifth">
+                <div class="column is-centered is-two-fifths">
                     <p>Subject</p>
-                    <select name="subject">
-                        <option>Other</option>
-                        <option>Infos</option> 
-                        <option>Request</option> 
-                        <option>Joining us</option>
-                    </select>
+                    <div class="select is-rounded">
+                        <select name="subject">
+                            <option>Other</option>
+                            <option>Infos</option> 
+                            <option>Request</option> 
+                            <option>Joining us</option>
+                        </select>
+                    </div>
                 </div>
             </div>
 
             <div class="columns is-centered is-gapless">
-                <div class="column is-centered">
+                <div class="column is-centered is-two-fifths">
                     <p>Message</p>
-                    <textarea name="message" placeholder="Write your message here"></textarea>
+                    <textarea class="textarea is-rounded" name="message" placeholder="Write your message here"></textarea>
                 </div>
             </div>
         </form>
